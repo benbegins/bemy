206777e5da349edff6382d03c568a796470e7ee0
benbegins <benoit.beghyn@gmail.com>
2021-04-10 19:28:58 +0200

    new polylang strings

--- functions.php
+++ functions.php
@@ -103,18 +103,31 @@ function projet_post_type() {
 add_action( 'init', 'projet_post_type', 0 );
 
 
-
-
 // POLYLANG
 add_action('init', function() {
     pll_register_string('contactezNous', 'Contactez-nous');
     pll_register_string('mentionsLegales', 'Mentions légales');
-    pll_register_string('siteVitrine', 'Site vitrine');
-    pll_register_string('siteEcommerce', 'Site e-commerce');
+    pll_register_string('frontEnd', 'Développement front-end');
+    pll_register_string('siteEcommerce', 'e-commerce');
     pll_register_string('identiteVisuelle', 'Identité visuelle');
     pll_register_string('papeterie', 'Papeterie');
     pll_register_string('kitSocialMedia', 'Kit réseaux sociaux');
     pll_register_string('projetSuivant', 'Projet suivant');
     pll_register_string("retourAccueil", "Retour à l'accueil");
     pll_register_string("voirLeSite", "Voir le site");
+    pll_register_string("accueilStudio", "Nous sommes un studio de création Barcelo-Nantais qui accompagne les agences et les marques dans la production d’expériences claires, esthétiques et durables.");
+    pll_register_string("nosDerniersProjets", "Nos derniers projets");
+    pll_register_string("nosDerniersArticles", "Nos derniers articles");
+    pll_register_string("introStudio_1", "Nous croyons en la force de l’esthétique et de l’authenticité pour créer un lien émotionnel et de confiance avec votre cible. Nous créons des identités visuelles et des sites clairs, uniques et durables, pour mieux vous raconter, et vous montrer sous votre meilleur jour.");
+    pll_register_string("introStudio_2", "Pour faire simple, on bosse bien.");
+    pll_register_string("citationStudio", "Nous collaborons avec les marques et les agences qui partagent nos valeurs humaines et l’amour du travail bien fait.");
+    pll_register_string("expertises", "Expertises");
+    pll_register_string("introExpertises", "Nos expertises sont parfaitement adaptées pour vous accompagner dans le cadre d’un lancement de marque, d’un rebranding, d’une création ou refonte de site.");
+    pll_register_string("derniersProjets", "Derniers projets");
+    pll_register_string("leStudio", "Le studio");
+    pll_register_string("leBlog", "Le blog");
+    pll_register_string("ditesNous", "Dites-nous");
+    pll_register_string("bonjour", "bonjour");
+    pll_register_string("contenu", "Contenu");
+    pll_register_string("identite", "Identité");
 });
